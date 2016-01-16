6083ad3e668f449043f3b273c910347fd23ce780
Tommy <spiel.hoelle@live.de>
2016-01-16 13:01:31 +0100

    added underscore and removed other themes

---
+++ wp-content/themes/amnesty/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package amnesty
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'amnesty' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'amnesty' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'amnesty' ), 'amnesty', '<a href="http://thomaskuhnert.com" rel="designer">Thomas Kuhnert</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
