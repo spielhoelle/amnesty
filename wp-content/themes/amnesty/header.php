3a22b4a021db4f6e820afa09477b980dde802c02
Tommy <spiel.hoelle@live.de>
2016-01-22 10:50:09 +0100

    scss - cleanup

--- wp-content/themes/amnesty/header.php
+++ wp-content/themes/amnesty/header.php
@@ -25,31 +25,30 @@
     <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'amnesty'); ?></a>
 
     <header id="masthead" class="site-header" role="banner">
-        <div class="top">
-            <div class="site-branding">
+
+        <div class="site-branding">
+            <?php
+            if (is_front_page() && is_home()) : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php else : ?>
+                <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                         rel="home"><?php bloginfo('name'); ?></a></p>
                 <?php
-                if (is_front_page() && is_home()) : ?>
-                    <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                              rel="home"><?php bloginfo('name'); ?></a></h1>
-                <?php else : ?>
-                    <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
-                                             rel="home"><?php bloginfo('name'); ?></a></p>
-                    <?php
-                endif;
+            endif;
 
-                $description = get_bloginfo('description', 'display');
-                if ($description || is_customize_preview()) : ?>
-                    <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-                    <?php
-                endif; ?>
-            </div><!-- .site-branding -->
+            $description = get_bloginfo('description', 'display');
+            if ($description || is_customize_preview()) : ?>
+                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                <?php
+            endif; ?>
+        </div><!-- .site-branding -->
 
-            <nav id="site-navigation" class="main-navigation" role="navigation">
-                <button class="menu-toggle" aria-controls="primary-menu"
-                        aria-expanded="false"><?php esc_html_e('Primary Menu', 'amnesty'); ?></button>
-                <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
-            </nav><!-- #site-navigation -->
-        </div>
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php esc_html_e('Primary Menu', 'amnesty'); ?></button>
+            <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+        </nav><!-- #site-navigation -->
 
 
         <?php if (($header_images = get_uploaded_header_images())): ?>
