a696ce93dceb60967107bd807c01a240b154cc06
Tommy <spiel.hoelle@live.de>
2016-01-25 12:40:10 +0100

    removed useless templates

--- wp-content/themes/amnesty/template-parts/section-kuenstler.php
+++
@@ -1,7 +0,0 @@
-<section id="section-<?php the_ID(); ?>" data-url="<?php the_permalink(); ?>"
-         tabindex="<?php echo $post->menu_order; ?>" rel="<?php echo $post->post_title; ?>">
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div>
-</section>
-<!-- #section-kuenstler.php## -->
