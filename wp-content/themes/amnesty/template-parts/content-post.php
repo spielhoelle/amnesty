390bcb05565f1f2e2dac6629f57bc94444e821e8
Tommy <spiel.hoelle@live.de>
2016-01-25 13:43:52 +0100

    news grid working, fixed headerπp

--- wp-content/themes/amnesty/template-parts/content-post.php
+++ wp-content/themes/amnesty/template-parts/content-post.php
@@ -27,28 +27,11 @@ $content_parts = get_extended($content);
         </a>
 
         <figcaption class="bubble">
-            <h1 class="entry-title"><?php echo get_the_title() ?></h1>';
+            <h1 class="entry-title"><?php echo get_the_title() ?></h1>
             <h2><?php echo $content_parts['main'] ?></h2>
             <a class="more-link" href="<?php the_permalink() ?>"> Mehr... </a>
         </figcaption>
     </figure>
 
-    <header class="entry-header">
-        <?php
-        echo '<h1>' . get_the_date() . '</h1>';
-        echo '<p>' . get_the_title() . '</p>';
-
-
-        echo '<a href="' . get_the_permalink() . '" class="more-link show">Mehr…</a>';
-        ?>
-    </header>
-    <!-- .entry-header -->
-
-    <div class="entry-content">
-        <a href="<?php the_permalink() ?>">
-            <img src="/wp-includes/images/blank.gif" style="background-image:url(<?php echo $img ?>)">
-        </a>
-    </div>
-    <!-- .entry-content -->
 </article>
 <!-- content-post.php frontsite -->
--- wp-content/themes/amnesty/template-parts/section-news.php
+++ wp-content/themes/amnesty/template-parts/section-news.php
@@ -1,20 +1,22 @@
 <section id="section-<?php the_ID(); ?>" data-url="<?php the_permalink(); ?>"
          tabindex="<?php echo $post->menu_order; ?>" rel="<?php echo $post->post_title; ?>">
-	<header class="entry-header">
-		<h2 class="entry-title"><?php the_title() ?></h2>
-	</header>
+    <header class="entry-header">
+        <h2 class="entry-title"><?php the_title() ?></h2>
+    </header>
 
-		<?php // switch WP to page for posts
-		$blog = new WP_Query( 'posts_per_page=5, page_id=' . get_option( 'page_for_posts' ) );
+    <div class="newswrapper">
+        <?php // switch WP to page for posts
+        $blog = new WP_Query('posts_per_page=5, page_id=' . get_option('page_for_posts'));
 
-		// loop through posts
-		while ( $blog->have_posts() ) : $blog->the_post();
-			get_template_part( 'template-parts/content', 'post' );
-		endwhile; ?>
+        // loop through posts
+        while ($blog->have_posts()) : $blog->the_post();
+            get_template_part('template-parts/content', 'post');
+        endwhile; ?>
 
-		<?php // restore original post data
-		wp_reset_postdata(); ?>
+        <?php // restore original post data
+        wp_reset_postdata(); ?>
+    </div>
 
-	<a class="more-link show" href="<?php echo get_permalink( get_page_by_path( 'alle-news' ) ) ?>">Mehr...</a>
+    <a class="more-link show" href="<?php echo get_permalink(get_page_by_path('alle-news')) ?>">Mehr...</a>
 </section>
 <!-- #section-news.php-## -->
