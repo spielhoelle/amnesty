fcc22b70ab31ec3ead09ffce1ba2e6ca4c2956bb
Thomas Kuhnert <thomas@wonderweblabs.io>
2016-04-24 12:46:30 +0200

    random improvement

--- wp-content/themes/amnesty/template-parts/content-home.php
+++ wp-content/themes/amnesty/template-parts/content-home.php
@@ -22,9 +22,7 @@ $the_query = new WP_Query('posts_per_page=6'); ?>
                           <?php icons();
                           echo get_the_title() ?>
                       </h1>
-                      <br>
                       <h2><?php echo $content_parts['main'] ?></h2>
-                      <br>
                       <a class="more-link" href="<?php the_permalink() ?>"> Mehr... </a>
                   </figcaption>
               </figure>
