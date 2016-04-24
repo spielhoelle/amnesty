0ff1cd3a9ae8a3dd0badb5270958c33c7f0632ef
Thomas Kuhnert <thomas@wonderweblabs.io>
2016-04-24 13:42:38 +0200

    nice header headlines

--- wp-content/themes/amnesty/js/functions.js
+++ wp-content/themes/amnesty/js/functions.js
@@ -1,24 +1,21 @@
-(function ($) {
+(function($) {
     var body = document.getElementsByTagName('body')[0];
-    $(document).ready(function (jQuery) {
-        /**
-         * slider
-         */
+    $(document).ready(function(jQuery) {
         $('.slider').bxSlider({
-            controls:    false,
-            mode:        'fade',
-            speed:       500,
-            pause:       10000,
-            nextText:    '<i class="fa fa-angle-right"></i>',
-            prevText:    '<i class="fa fa-angle-left"></i>',
-            auto:        true,
+            controls: false,
+            mode: 'fade',
+            speed: 500,
+            pause: 10000,
+            nextText: '<i class="fa fa-angle-right"></i>',
+            prevText: '<i class="fa fa-angle-left"></i>',
+            auto: true,
             pagerCustom: '.custom-pager',
 
-            onSlideAfter: function (currentSlideNumber, totalSlideQty, currentSlideHtmlObject) {
+            onSlideAfter: function(currentSlideNumber, totalSlideQty, currentSlideHtmlObject) {
                 $('.active-slide').removeClass('active-slide');
                 $('.slider > figure').eq(currentSlideHtmlObject).addClass('active-slide')
             },
-            onSliderLoad: function () {
+            onSliderLoad: function() {
                 $('.slider > figure').eq(0).addClass('active-slide')
             }
         });
@@ -34,54 +31,62 @@
          * show newsletter popup after X seconds and just once overall
          */
         if (localStorage.getItem('popState') != 'shown' && jQuery(window).width() > 1200) {
-
-            setTimeout(function () {
-                $(".site-footer > #nlpopup").addClass('active')
-            }, 20000);
-
+            setTimeout(function() {
+                openPopup()
+            }, 1000);
             localStorage.setItem('popState', 'shown')
         }
 
         //close functions
-        $('#nlpopup .close').click(function (e) // You are clicking the close button
-        {
-            $(".site-footer > #nlpopup").fadeOut().removeClass("active");
+        $('#nlpopup .close').click(function(e) {
+            closePopup();
         });
 
         $(document).keydown(function(e) {
             // ESCAPE key pressed
             if (e.keyCode == 27) {
-              $(".site-footer > #nlpopup").fadeOut().removeClass("active");
+                closePopup()
             }
         });
 
-        $("body").click(function () {
-            $(".site-footer > #nlpopup").fadeOut().removeClass("active");
+        $("body").click(function() {
+            closePopup()
         });
 
-//      Prevent events from getting pass .popup
-        $("#nlpopup .wpcf7").click(function (e) {
+
+        $("#nlpopup .wpcf7").click(function(e) {
             e.stopPropagation();
         });
 
+        closePopup = function() {
+          $(".site-footer > #nlpopup").fadeOut().removeClass("active");
+          $(body).removeClass('nl-open')
+        }
+        openPopup = function() {
+          $(".site-footer > #nlpopup").addClass('active')
+          $(body).addClass('nl-open')
+        }
 
-        $('#menu-search .fa').click(function(){
-          if($('#menu-search .search-form').is(':visible')){
-              $('#menu-search .search-form').hide()
+        /**
+         * header navigation SEARCH
+         */
+        $('#menu-search .fa').click(function() {
+            if ($('#menu-search .search-form').is(':visible')) {
+                $('#menu-search .search-form').hide()
             } else {
-            $('#menu-search .search-form').show()
-            $('#menu-search .search-form .search-field').focus()
+                $('#menu-search .search-form').show()
+                $('#menu-search .search-form .search-field').focus()
 
-          }
+            }
         })
 
-        $('body').click(function(){
-          if($('#menu-search .search-form').is(':visible')){
-            $('#menu-search .search-form').hide()
-          }
+        $('body').click(function() {
+            if ($('#menu-search .search-form').is(':visible')) {
+                $('#menu-search .search-form').hide()
+            }
         })
 
-        $('#menu-search').click(function (e) {
+        $('#menu-search').click(function(e) {
             e.stopPropagation();
         });
 
