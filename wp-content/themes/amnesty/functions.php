ad024ad96bac1277b7fc6de68308da0701987cff
Thomas Kuhnert <thomas@wonderweblabs.io>
2016-06-09 12:37:18 +0200

    antispam function

--- wp-content/themes/amnesty/functions.php
+++ wp-content/themes/amnesty/functions.php
@@ -231,3 +231,23 @@ function gridloop() {
    endwhile;
 }
 add_filter( 'widget_text', 'do_shortcode' );
+
+
+
+
+/**
+ * Hide email from Spam Bots using a shortcode.
+ *
+ * @param array  $atts    Shortcode attributes. Not used.
+ * @param string $content The shortcode content. Should be an email address.
+ *
+ * @return string The obfuscated email address.
+ */
+function wpcodex_hide_email_shortcode( $atts , $content = null ) {
+	if ( ! is_email( $content ) ) {
+		return;
+	}
+
+	return '<a href="mailto:' . antispambot( $content ) . '">' . antispambot( $content ) . '</a>';
+}
+add_shortcode( 'email', 'wpcodex_hide_email_shortcode' );
