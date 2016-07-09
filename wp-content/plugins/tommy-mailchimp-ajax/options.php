3a10169aa0a97577b90a8476c1885998e740678d
Thomas Kuhnert <flughafen65@live.de>
2016-07-09 13:00:10 +0200

    options got saved

--- wp-content/plugins/tommy-mailchimp-ajax/options.php
+++ wp-content/plugins/tommy-mailchimp-ajax/options.php
@@ -8,6 +8,8 @@ Author: Thomas Kuhnert
 Author URI:
 */
 
+
+
 add_action('admin_menu', 'test_plugin_setup_menu');
  function test_plugin_setup_menu(){
     add_menu_page( 'Mailchimp-Ajax', 'Mailchimp-Ajax', 'manage_options', 'test-plugin', 'backend_options_page' );
@@ -26,19 +28,44 @@ add_action( 'admin_init', 'wporg_register_settings' );
 
 
 function backend_options_page(){
-	?>
-	<h1>WordPress Extra Post Info</h1>
-	  <form method="post" action="options.php">
-	    <?php settings_fields( 'extra-post-info-settings' ); ?>
-	    <?php do_settings_sections( 'extra-post-info-settings' ); ?>
-	    Api Key:
-       <input type="text" name="api_key" value="<?php echo get_option( 'api_key' ); ?>"/>
-	    List Id:
-	    <input type="text" name="list_id" value="<?php echo get_option( 'list_id' ); ?>"/>
-	    <?php submit_button(); ?>
-	  </form>
-
-<?php
+    ?>
+	    <div class="wrap">
+	    <h1>Theme Panel</h1>
+	    <form method="post" action="options.php">
+	        <?php
+	            settings_fields("section");
+	            do_settings_sections("theme-options");      
+	            submit_button(); 
+	        ?>          
+	    </form>
+		</div>
+	<?php
 }
 
 
+function display_twitter_element()
+{
+	?>
+    	<input type="text" name="api_key" id="api_key" value="<?php echo get_option('api_key'); ?>" />
+    <?php
+}
+
+function display_facebook_element()
+{
+	?>
+    	<input type="text" name="list_id" id="list_id" value="<?php echo get_option('list_id'); ?>" />
+    <?php
+}
+
+function display_theme_panel_fields()
+{
+	add_settings_section("section", "All Settings", null, "theme-options");
+	
+	add_settings_field("api_key", "Mailchimp Api-Key", "display_twitter_element", "theme-options", "section");
+    add_settings_field("list_id", "Mailchimp List-ID", "display_facebook_element", "theme-options", "section");
+
+    register_setting("section", "api_key");
+    register_setting("section", "list_id");
+}
+
+add_action("admin_init", "display_theme_panel_fields");
