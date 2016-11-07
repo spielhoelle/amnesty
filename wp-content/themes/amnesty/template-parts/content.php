25f14cb8a0a1a8a75a47c18b8e298a92d21d7589
Thomas Kuhnert <flughafen65@live.de>
2016-11-07 17:27:08 +0100

    final responsive stuff

--- wp-content/themes/amnesty/template-parts/content.php
+++ wp-content/themes/amnesty/template-parts/content.php
@@ -20,17 +20,18 @@ $content_parts = get_extended($content);
 ?>
 <article data-url="<?php the_permalink(); ?>" id="post-<?php the_ID(); ?>" <?php post_class($classes); ?>>
 
-     <figure class="gridfigure">
+     <figure class="gridfigure ratiocontainer">
+       <div class="ratio">
+          <a href="<?php the_permalink() ?>">
+              <img src="/wp-includes/images/blank.gif" style="background-image:url(<?php echo $img ?>)">
+          </a>
 
-        <a href="<?php the_permalink() ?>">
-            <img src="/wp-includes/images/blank.gif" style="background-image:url(<?php echo $img ?>)">
-        </a>
-
-        <span class="icons gridicon"><?php icons(true, true); ?></span>
-        <figcaption>
-          <div>
-            <h1 class="entry-title">
-              <?php echo get_the_title() ?></h1>
+          <span class="icons gridicon"><?php icons(true, true); ?></span>
+          <figcaption>
+            <div>
+              <h1 class="entry-title">
+                <?php echo get_the_title() ?></h1>
+            </div>
           </div>
         </figcaption>
     </figure>
