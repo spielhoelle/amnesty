e1592046d6126bf4180e0f272e5e7b5a159ba58d
Thomas Kuhnert <flughafen65@live.de>
2016-11-08 21:50:12 +0100

    add content super headline to tinymce

--- wp-content/themes/amnesty/inc/tinymce.php
+++ wp-content/themes/amnesty/inc/tinymce.php
@@ -8,23 +8,28 @@ add_filter('mce_buttons_2', 'wpb_mce_buttons_2');
 
 
 # Callback function to filter the MCE settings
-function my_mce_before_init_insert_formats( $init_array ) {  
+function my_mce_before_init_insert_formats( $init_array ) {
     // Define the style_formats array
-    $style_formats = array(  
+    $style_formats = array(
         // Each array child is a format with it's own settings
-        array(  
-            'title' => 'Button (nur auf links anwendbar)',  
-            'selector' => 'a',  
-            'classes' => 'content-button'             
+        array(
+            'title' => 'Button (nur auf links anwendbar)',
+            'selector' => 'a',
+            'classes' => 'content-button'
+        ),
+        array(
+            'title' => 'Super Headline',
+            'selector' => 'h1',
+            'classes' => 'entry-title content'
         )
-    );  
+    );
     // Insert the array, JSON ENCODED, into 'style_formats'
-    $init_array['style_formats'] = json_encode( $style_formats );  
+    $init_array['style_formats'] = json_encode( $style_formats );
 
-    return $init_array;  
+    return $init_array;
 
-} 
-// Attach callback to 'tiny_mce_before_init' 
+}
+// Attach callback to 'tiny_mce_before_init'
 add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
 
 
